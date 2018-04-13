e8e737f58b499b6421ca120e48fc3ad1b7415eb4
jlehocz <jozsef@lehocz.hu>
2018-04-13 21:44:46 +0200

    complete, without mp3

--- app/src/main/java/com/example/jlehocz/mediaplayer/MainActivity.java
+++ app/src/main/java/com/example/jlehocz/mediaplayer/MainActivity.java
@@ -1,7 +1,14 @@
 package com.example.jlehocz.mediaplayer;
 
+import android.content.Intent;
+import android.media.MediaPlayer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+
+import java.io.IOException;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +16,52 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button playButton = (Button) findViewById(R.id.play);
+        Button pauseButton = (Button) findViewById(R.id.pause);
+        Button stopButton = (Button) findViewById(R.id.stop);
+
+        final MediaPlayer mp = MediaPlayer.create(this, R.raw.nib);
+
+
+        playButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                mp.start();
+            }
+
+        });
+
+        pauseButton.setOnClickListener(new View.OnClickListener() {
+
+            public void onClick(View v) {
+                if (mp.isPlaying()) {
+                    mp.pause();
+                    int length = mp.getCurrentPosition();
+                    mp.seekTo(length);
+                }
+
+            }
+        });
+
+        stopButton.setOnClickListener(new View.OnClickListener() {
+
+            public void onClick(View v) {
+                if (mp.isPlaying()) {
+                    mp.stop();
+
+                    try {
+                        mp.prepare();
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+
+
+                }
+
+            }
+        });
+
     }
+
+
 }
